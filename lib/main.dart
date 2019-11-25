23e6f7e63171068359e5b161d4a146c746e35335
Galozy, Adrian <adrian.galozy@student.fhws.de>
2019-11-25 15:47:55 +0100

    change appbar-color to studiAppGreen, add Icon in the Top left Corner

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'Colors.dart';
+
 // The main function is the starting point for all our Flutter apps.
 void main() {
   runApp(
@@ -7,9 +8,16 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
-          title: Text('FIW Studi App'),
-          backgroundColor: Colors.green,
-        ),
+            title: Text('FIW Studi App'),
+            backgroundColor: studiAppGreen,
+            leading: IconButton(
+                icon: Icon(
+                  Icons.menu,
+                  semanticLabel: 'menu',
+                ),
+                onPressed: () {
+                  print('menu button');
+                })),
         body: Center(
           child: Image(
             image: AssetImage(''),
