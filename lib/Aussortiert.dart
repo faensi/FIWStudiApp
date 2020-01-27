5cf3fc75a267236df10ee1d471d93f03bb0037f0
Imling, Raol <raol-florian.imling@freenet.de>
2020-01-27 15:10:14 +0100

    Erste Fassung für API Http Abfragen

---
+++ lib/Aussortiert.dart
@@ -0,0 +1,19 @@
+
+static String getCourseFromMatrikelNumber(int matrikelNumber){
+String studiengang =  "error: matrikel-number format is not correct.";
+if(matrikelNumber.toString().length==7) {
+if (matrikelNumber >= 50000 || matrikelNumber < 51000) {
+studiengang = "bwi";
+}
+else if (matrikelNumber >= 51000 || matrikelNumber < 52000) {
+studiengang = "bin";
+}
+else if (matrikelNumber >= 60000 || matrikelNumber < 61000){
+studiengang = "bec";
+}
+return studiengang;
+}
+else{
+return studiengang;
+}
+}
