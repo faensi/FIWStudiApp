0533ce85a38e6ea22c66744460e0a8c06fd5d235
Hubertus Knobling <hubertus.knobling@student.fhws.de>
2020-01-28 20:19:16 +0100

    add imports; add networking getCData(); change Event onTap to getCData + Display; add this.getCData() to initState

--- lib/Coffee/Button.dart
+++ lib/Coffee/Button.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:FIW_Studi_App/KaffeeView.dart';
+import 'package:flutter/material.dart';
+import 'package:FIW_Studi_App/Coffee/Post.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class CoffeeButton extends StatefulWidget {
   @override
@@ -10,12 +17,30 @@ class CoffeeButton extends StatefulWidget {
 class CoffeeButtonState extends State<CoffeeButton> {
   //final String _assetPath;
 
-//  CoffeeBannerState(this._assetPath);
+  //  CoffeeBannerState(this._assetPath);
   String image1 = "Images/Coffee_background.png";
-  String status = "Kaffee gemacht";
+  String status = "Kaffee gemacht!";
+  final String url =
+      "https://apistaging.fiw.fhws.de/studi-app/api/coffee-machine/getCurrentState";
+  var data;
+  var state;
+  var lastTime;
+  var userName;
+
+  Future<String> getCData() async {
+    http.Response response = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+    data = jsonDecode(response.body);
+    state = data["state"].toString();
+    lastTime = data["statusTime"];
+    userName = data[
+        "userName"]; //momentan nicht in Verwendung; hier Aufgrund der Datenabnk
+    return "Success!";
+  }
+
   @override
   Widget build(BuildContext context) {
-    // erzeugt Container mit größe 200x200 Pixel für den KaffeeButton
+    // erzeugt Container mit Größe 200x200 Pixel für den KaffeeButton
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.center,
@@ -25,18 +50,30 @@ class CoffeeButtonState extends State<CoffeeButton> {
             height: 200,
             width: 200,
           ),
-          // Knopf ändert auf Druck den Status und soll diesen Später an die Datenbank senden
           child: GestureDetector(
             onTap: () => setState(() {
-              if (image1 == "Images/Coffee_background.png") {
+              getCData();
+              if (state == "10") {
                 image1 = "Images/Coffeeimage.png";
-                status = "Kaffee gemacht";
-              } else if (image1 == "Images/Coffeeimage.png") {
+                status = "Kaffee gemacht" +
+                    "\n" +
+                    DateTime.parse(lastTime).toString();
+              } else if (state == "20") {
                 image1 = "Images/KaffeGrey.png";
-                status = "Kaffee leer";
-              } else if (image1 == "Images/KaffeGrey.png") {
+                status =
+                    "Kaffee leer" + "\n" + DateTime.parse(lastTime).toString();
+              } else if (state == "30") {
                 image1 = "Images/Coffee_background.png";
-                status = "Kaffeemaschine aus";
+                status = "Kaffeemaschine aus" +
+                    "\n" +
+                    DateTime.parse(lastTime).toString();
+              } else if (state == "40") {
+                image1 = "Images/Coffee_background.png";
+                status = "Kaffeemaschine defekt" +
+                    "\n" +
+                    DateTime.parse(lastTime).toString();
+              } else {
+                print("Error");
               }
             }),
             child: Image.asset(image1),
@@ -53,5 +90,11 @@ class CoffeeButtonState extends State<CoffeeButton> {
         )
       ],
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getCData();
   }
 }
