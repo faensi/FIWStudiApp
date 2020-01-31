14b6709995a97542f0d85ed4044508d35a2f49ff
Galozy, Adrian <adrian.galozy@student.fhws.de>
2020-01-31 21:26:38 +0100

    change Vorlesungsplanerview.dart, add Listbuilder Widgets

--- lib/VorlesungsplanerView.dart
+++ lib/VorlesungsplanerView.dart
@@ -1,61 +1,51 @@
+import 'package:FIW_Studi_App/Vorlesungsplaner/VorlesungsplanerSemester.dart';
 import 'package:flutter/material.dart';
 import 'package:FIW_Studi_App/UI/Colors.dart';
 
 class VorlesungsplanerView extends StatelessWidget {
-  static const double _hPad = 16;
+  final List<String> studiengaenge = ["BEC", "INF", "WINF", "MINF"];
 
   @override
   Widget build(BuildContext context) {
-    List<SizedBox> _buildCards(int count) {
-      List<SizedBox> boxes = List.generate(
-          count,
-          (int index) => SizedBox(
-                height: 120,
-                child: Card(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      ListTile(
-                        title: Text('Vorlesungstitel'),
-                        subtitle: Text('Kurze beschreibung'),
-                        leading: Icon(
-                          Icons.add_to_photos,
-                          color: studiAppGreen,
-                          size: 50,
-                        ),
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                      )
-                    ],
-                  ),
-                ),
-              ));
-
-      return boxes;
-    }
-
-    return MaterialApp(
-      title: 'VorlesungsplanerView',
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text('Vorlesungsplaner'),
-          backgroundColor: studiAppGreen,
-          leading: IconButton(
-            icon: Icon(
-              Icons.arrow_back,
-              semanticLabel: 'Back',
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Wähle deinen Studiengang aus"),
+        backgroundColor: studiAppGreen,
+      ),
+      body: ListView.builder(
+        itemCount: studiengaenge.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            child: Center(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context, // Hier Semestervariable abändern.
+                          MaterialPageRoute(
+                            builder: (context) => VorlesungsplanerSemester(),
+                          ),
+                        );
+                      },
+                      child: Card(
+                        child: Container(
+                            padding: EdgeInsets.all(15.0),
+                            child: Row(
+                              children: <Widget>[
+                                Text("Name: "),
+                                Text(studiengaenge[index],
+                                    style: TextStyle(
+                                        fontSize: 18.0, color: Colors.black87)),
+                              ],
+                            )),
+                      ),
+                    ),
+                  ]),
             ),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-        ),
-        body: ListView(
-          children: _buildCards(10),
-        ),
+          );
+        },
       ),
     );
   }
