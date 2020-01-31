5bc59ccaa1bc543cbb5b39bc7c72111dfc725010
Hudson <denesioanag@yahoo.com>
2020-01-31 19:57:45 +0100

    Study Rooms UI DONE backend in progress

--- lib/UI/main.dart
+++ lib/UI/main.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:FIW_Studi_App/UI/app.dart';
+import 'package:FIW_Studi_App/UI/studyrooms.dart';
+import '../Startview.dart';
 
 // The main function is the starting point for all our Flutter apps.
 void main() {
-  runApp(App());
+  runApp(
+    MaterialApp(
+      title: 'FIW Studi App',
+      home: Startview(),
+    ),
+  );
+}
+
+void rooms() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Study Rooms',
+      theme: ThemeData(
+        primaryColor: new Color(0xFF66BB6A),
+        backgroundColor: new Color(0xFFC8E6C9),
+      ),
+      home: Rooms(),
+    );
+  }
 }
