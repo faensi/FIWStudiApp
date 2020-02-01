ca36555ec419513bf7b250e742b650eee1deac62
Hubertus Knobling <hubertus.knobling@student.fhws.de>
2020-02-01 18:31:47 +0100

    modify postRequest to take Parameter

--- lib/Coffee/Post.dart
+++ lib/Coffee/Post.dart
@@ -2,12 +2,15 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<http.Response> postRequest() async {
+Future<http.Response> postRequest(int postState) async {
   var url = 'https://apistaging.fiw.fhws.de/studi-app/api/coffee-machine/';
   var ISO8601time =
-      DateTime.now().toIso8601String().substring(0, 19) + "+01:00";
-  //ISO8601 implementation only meant for +01:00 timezone
-  Map data = {"state": 20, "statusTime": ISO8601time, "userName": "TestUserX"};
+      DateTime.now().toIso8601String().substring(0, 19) + "+00:00";
+  Map data = {
+    "state": postState,
+    "statusTime": ISO8601time,
+    "userName": "TestUserX"
+  };
   //noch hardcoded bitte aendern
 
   //encode Map to JSON
