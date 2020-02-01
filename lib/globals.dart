e017cedf101b65d2ba509a4f9fde96485b9e2722
Hubertus Knobling <hubertus.knobling@student.fhws.de>
2020-02-01 15:36:58 +0100

    relocate variables for data called from API to globals

--- lib/globals.dart
+++ lib/globals.dart
@@ -5,3 +5,9 @@ int semester = 0;
 String degreeProgramm = "";
 int offset = 0;
 int insertSize = 10;
+
+//variables for Button
+Map<String, dynamic> cData;
+String cState;
+DateTime cLastTime;
+String cUserName;
