7b170786bfc7fead2b828a62195711c1f9a1efbb
Hubertus Knobling <hubertus.knobling@student.fhws.de>
2020-02-02 20:17:10 +0100

    add class for testing

---
+++ lib/UI/newLoginForm.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/foundation.dart';
+
+class newLoginForm extends StatelessWidget {
+  const newLoginForm({
+    Key key,
+    @required this.onSubmit,
+  }) : super(key: key);
+
+  final VoidCallback onSubmit;
+  static final TextEditingController _user = new TextEditingController();
+  static final TextEditingController _pass = new TextEditingController();
+
+  String get username => _user.text;
+  String get password => _pass.text;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      children: <Widget>[
+        new TextField(
+          controller: _user,
+          decoration: new InputDecoration(hintText: 'kNumber'),
+        ),
+        new TextField(
+          controller: _pass,
+          decoration: new InputDecoration(hintText: 'password'),
+        ),
+        new RaisedButton(child: new Text('Submit'), onPressed: onSubmit)
+      ],
+    );
+  }
+}
