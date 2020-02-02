73eaab056919aac1239179c078d947491cbd924f
Hubertus Knobling <hubertus.knobling@student.fhws.de>
2020-02-02 22:13:30 +0100

    make new function postNews(..)

--- lib/Networking/newsNet.dart
+++ lib/Networking/newsNet.dart
@@ -1,1 +1,29 @@
-void postNews(String title, String text) {}
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:FIW_Studi_App/globals.dart' as globals;
+
+Future<http.Response> postNews(String title, String text) async {
+  var url = 'https://apistaging.fiw.fhws.de/studi-app/api/news';
+  String username = globals.kNumber;
+  String password = globals.password;
+
+  String basicAuth =
+      'Basic ' + base64Encode(utf8.encode('$username:$password'));
+  Map data = {
+    "image": "",
+    "text": text,
+    "time": globals.getIso8601Time(),
+    "title": title,
+    "userName": username
+  };
+  var body = json.encode(data);
+  var response = await http.post(url,
+      headers: {
+        HttpHeaders.authorizationHeader: basicAuth,
+        "Content-Type": "application/json"
+      },
+      body: body);
+  return response;
+}
