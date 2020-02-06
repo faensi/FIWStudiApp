72d88659d99ea1bfd7ac5ddb4e212e0ae6153e93
Galozy, Adrian <adrian.galozy@student.fhws.de>
2020-02-06 11:17:14 +0100

    merge

--- lib/Rooms/bookings.dart
+++ lib/Rooms/bookings.dart
@@ -1,13 +1,15 @@
-import 'package:FIW_Studi_App/Model/Booking.dart';
 import 'package:flutter/material.dart';
+
+import 'Model/booking.dart';
 
 class RadioWidgetDemo extends StatefulWidget {
   static Future<List<Booking>> bookings;
   static List<Booking> bookingDetails;
 
-  RadioWidgetDemo(Future<List<Booking>> bookings,  List<Booking> bookingDetails){
-  RadioWidgetDemo.bookings = bookings;
-  RadioWidgetDemo.bookingDetails = bookingDetails;
+  RadioWidgetDemo(
+      Future<List<Booking>> bookings, List<Booking> bookingDetails) {
+    RadioWidgetDemo.bookings = bookings;
+    RadioWidgetDemo.bookingDetails = bookingDetails;
   }
 
   final String title = "Today's bookings";
@@ -17,48 +19,49 @@ class RadioWidgetDemo extends StatefulWidget {
 }
 
 class RadioWidgetDemoState extends State<RadioWidgetDemo> {
-
   int selectedRadioTile;
 
   @override
-  void initState () {
+  void initState() {
     super.initState();
-    selectedRadioTile= 0;
+    selectedRadioTile = 0;
   }
 
-  setSelectedRadioTile (int val) {
+  setSelectedRadioTile(int val) {
     setState(() {
-      selectedRadioTile= val;
+      selectedRadioTile = val;
     });
-
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        title: Text('Study Rooms',
+        title: Text(
+          'Study Rooms',
           style: TextStyle(
             color: Colors.black,
           ),
         ),
-
       ),
-      body:
-      FutureBuilder(
+      body: FutureBuilder(
         future: RadioWidgetDemo.bookings,
         builder: (context, projectSnap) {
           return ListView.builder(
-            itemCount: RadioWidgetDemo.bookingDetails == null ? 0 : RadioWidgetDemo.bookingDetails.length,
+            itemCount: RadioWidgetDemo.bookingDetails == null
+                ? 0
+                : RadioWidgetDemo.bookingDetails.length,
             itemBuilder: (context, index) {
               return Card(
-                child: ListTile(
-                  title: Text(RadioWidgetDemo.bookingDetails[index].description + "\n",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 25.0, color: Colors.black)),
-                  subtitle: Text(RadioWidgetDemo.bookingDetails[index].description))
-                  );
+                  child: ListTile(
+                      title: Text(
+                          RadioWidgetDemo.bookingDetails[index].description +
+                              "\n",
+                          textAlign: TextAlign.center,
+                          style:
+                              TextStyle(fontSize: 25.0, color: Colors.black)),
+                      subtitle: Text(
+                          RadioWidgetDemo.bookingDetails[index].description)));
             },
           );
         },
